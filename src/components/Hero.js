b23ec102ca0d87068cfa87a774b3c2eecdd7cf7a
Devjm <120639463+Devanathanjm@users.noreply.github.com>
2026-03-23 19:24:50 +0530

    Change paragraph animation from fade-left to fade-up

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className='max-w-[949] text-white mx-auto text-center py-14'>
                 <h1 className='text-[36px] md:text-[70px] leading-[132%] font-semibold mb-7'> <span className='bg-[linear-gradient(90deg,#71BF44_16.83%,#6FC1FF_67.79%,#71BF44_100%)] bg-clip-text text-transparent' data-aos="fade-up" data-aos-delay="200"> Inventory that speaks</span>  your
                     units at every stage</h1>
-                <p className='text-[20px] md:text-[25px] max-w-[718] mx-auto mb-7 leading-[144%]' data-aos="fade-left" data-aos-duration="250">When units are clear, mistakes drop, cycle counts shrink, and profit improves.</p>
+                <p className='text-[20px] md:text-[25px] max-w-[718] mx-auto mb-7 leading-[144%]' data-aos="fade-up" data-aos-delay="200">When units are clear, mistakes drop, cycle counts shrink, and profit improves.</p>
                 <div className='text-[16px] md:text-[18px] max-w-[797] mx-auto mb-9 border border-[#1ECF6A] rounded-[15px] p-3'>
                     <span data-aos="zoom-in-up" data-aos-duration="250"> Buy in one unit, stock in another, sell in a third. We convert it for you so counts stay right and orders move fast.</span>
                 </div>
