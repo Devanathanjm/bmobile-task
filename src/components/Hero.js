1d64467bc5294e17f0b272f4513cf6d1c7b4d4b1
Devanathan-cg <devanathan@codeglo.com>
2026-03-23 19:15:18 +0530

    animated issue

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
         <section className="bg-[url('/images/hero-banner.png')] bg-cover bg-center md:h-[680] md:pt-16 md:pb-20 pt-10 overflow-hidden">
             <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
             <div className='max-w-[949] text-white mx-auto text-center py-14'>
-                <h1 className='text-[36px] md:text-[70px] leading-[132%] font-semibold mb-7'> <span className='bg-[linear-gradient(90deg,#71BF44_16.83%,#6FC1FF_67.79%,#71BF44_100%)] bg-clip-text text-transparent' data-aos="fade-up" data-aos-duration="400"> Inventory that speaks</span>  your
+                <h1 className='text-[36px] md:text-[70px] leading-[132%] font-semibold mb-7'> <span className='bg-[linear-gradient(90deg,#71BF44_16.83%,#6FC1FF_67.79%,#71BF44_100%)] bg-clip-text text-transparent' data-aos="fade-up" data-aos-delay="200"> Inventory that speaks</span>  your
                     units at every stage</h1>
                 <p className='text-[20px] md:text-[25px] max-w-[718] mx-auto mb-7 leading-[144%]' data-aos="fade-left" data-aos-duration="250">When units are clear, mistakes drop, cycle counts shrink, and profit improves.</p>
                 <div className='text-[16px] md:text-[18px] max-w-[797] mx-auto mb-9 border border-[#1ECF6A] rounded-[15px] p-3'>
